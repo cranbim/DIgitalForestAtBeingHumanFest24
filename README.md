899d721ea752c21d5342801a55967ed63e1e7e31
Dave Webb <dave.webb@cranbim.com>
2024-11-15 07:41:49 +0000

    preamble and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+#Digital Forest November 2024
+Created for the Being Human Festival 2024, "Trees, Food and You" event
+Created in p5.js by Dave Webb @crispysmokedweb
+You may freely use and develop from this code, but you must attribute the original author as Dave Webb
+Images and sounds are donated by audience members from the festival, who cannot be individually recognised here
+
+place sound files (must be .wav) to be included in the givenSounds folder, given a sequential filename gs000-gsnnn (eg. gs002.wav) and update the global variable numGivenSounds to reflect the number to be included
+
+place image file (must be .png) to be included in the givenSounds folder, given a sequential filename gi000-ginnn (eg. gi002.png) and update the global variable numGivenImages to reflect the number to be included
+
+gs and gi file numbers must be sequential from gs0#00 and counting on. gaps will cause load errors
--- sketch.js
+++ sketch.js
@@ -1,12 +1,33 @@
+//Digital Forest November 2024
+//Created for the Being Human Festival 2024, "Trees, Food and You" event
+//Created in p5.js by Dave Webb @crispysmokedweb
+//You may freely use and develop from this code, but you must attribute 
+//the original author as Dave Webb
+//Images and sounds are donated by audience members from the festival, who 
+//cannot be individually recognised here
+
+// place sound files (must be .wav) to be included in the givenSounds folder, 
+// given a sequential filename gs000-gsnnn (eg. gs002.wav) and update the global 
+// variable numGivenSounds to reflect the number to be included
+
+// place image file (must be .png) to be included in the givenSounds folder, 
+// given a sequential filename gi000-ginnn (eg. gi002.png) and update the global 
+// variable numGivenImages to reflect the number to be included
+
+//gs and gi file numbers must be sequential from gs000 and counting on.
+//gaps will cause load errors
+
+//variables to modify to reflect the media files to include
+let numGivenImages=5
+let numGivenSounds=6
+
 let leaves=[]
 let numLeaves=100
 let bgImage
 let leafShapes=[]
 let numLeafShapes=4
 let givenImages=[]
-let numGivenImages=5
 let givenSounds=[]
-let numGivenSounds=6
 let bgScale=1
 
 function preload(){
